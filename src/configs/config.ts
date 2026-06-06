4f63394d3689b68c3ce6fdd7dc1b960db0f213c1
yashrajt0001 <yeshrajteli12345@gmail.com>
2026-06-06 12:24:53 +0530

    change

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,6 +1,6 @@
 const CONFIG = {
   APP_NAME: "Madawatsab",
-  // API_BASE_URL: "https://madawatsab-backend.onrender.com/api/admin/",
+  API_BASE_URL: "https://madawatsab-backend.onrender.com/api/admin/",
   // API_BASE_URL: "http://localhost:3001/api/admin/",
 };
 
